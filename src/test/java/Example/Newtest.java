0f9ae4a1c61bed066b4be719f96ffe917179a641
dashr <dashr@LAPTOP-360P3OQH>
2022-07-17 22:34:33 +0530

    First Commit..

---
+++ src/test/java/Example/Newtest.java
@@ -0,0 +1,32 @@
+package Example;		
+
+import org.openqa.selenium.By;		
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+import org.testng.Assert;		
+import org.testng.annotations.Test;	
+import org.testng.annotations.BeforeTest;	
+import org.testng.annotations.AfterTest;		
+public class Newtest {		
+	    private WebDriver driver;		
+		@Test				
+		public void testEasy() {	
+			driver.get("http://demo.guru99.com/test/guru99home/");  
+			driver.manage().window().maximize();
+			String title = driver.getTitle();				 
+			Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
+		}	
+		@BeforeTest
+		public void beforeTest() {
+			System.setProperty("webdriver.chrome.driver","C:\\Users\\dashr\\Driver\\chromedriver_win32\\chromedriver.exe");
+			 driver = new ChromeDriver();
+		    
+		    
+		}		
+		@AfterTest
+		public void afterTest() {
+			driver.quit();			
+		}		
+}	
